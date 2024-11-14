e7ae30c26d1da33c0169af49fc6b35a9fe83bdf5
Christopher Poile <cpoile@gmail.com>
2024-11-13 23:33:49 -0500

    up to day 5, needs a bit of cleaning in main.

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((jai-mode . ((custom-compile-cmd . "c:/Users/Chris/git/jai/handmade_jai/make.bat")
+              (custom-run-all-cmd . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe")
+              (custom-run-test-cmd . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe test"))))
---
+++ make.bat
@@ -0,0 +1,5 @@
+@echo off
+
+if not exist c:\Users\Chris\git\jai\handmade_jai\build mkdir c:\Users\Chris\git\jai\handmade_jai\build
+
+jai c:\Users\Chris\git\jai\handmade_jai\src\main.jai -output_path c:\Users\Chris\git\jai\handmade_jai\build -exe handmade
