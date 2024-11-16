8fefff28e6f699d57dbcf888666f546e56e054e1
Christopher Poile <cpoile@gmail.com>
2024-11-16 15:12:41 -0500

    end of day 005; cleaned up main; modules_additions; new build

--- make.bat
+++ make.bat
@@ -2,4 +2,4 @@
 
 if not exist c:\Users\Chris\git\jai\handmade_jai\build mkdir c:\Users\Chris\git\jai\handmade_jai\build
 
-jai c:\Users\Chris\git\jai\handmade_jai\src\main.jai -output_path c:\Users\Chris\git\jai\handmade_jai\build -exe handmade
+jai c:\Users\Chris\git\jai\handmade_jai\build.jai -output_path c:\Users\Chris\git\jai\handmade_jai\build
