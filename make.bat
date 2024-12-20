95e22eb10d48d0d2aade794d4d3c2908d20a2fd2
Christopher Poile <cpoile@gmail.com>
2024-12-19 23:29:20 -0500

    day 021; hot code reloading

--- make.bat
+++ make.bat
@@ -1,5 +1,4 @@
 @echo off
 
-if not exist c:\Users\Chris\git\jai\handmade_jai\build mkdir c:\Users\Chris\git\jai\handmade_jai\build
-
-jai c:\Users\Chris\git\jai\handmade_jai\build.jai -output_path c:\Users\Chris\git\jai\handmade_jai\build
+jai c:\Users\Chris\git\jai\handmade_jai\build_win32_handmade.jai
+jai c:\Users\Chris\git\jai\handmade_jai\build_handmade.jai
