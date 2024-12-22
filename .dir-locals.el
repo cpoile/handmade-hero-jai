150128090912cdbd68921a508aff8776b4ad1b89
Christopher Poile <cpoile@gmail.com>
2024-12-21 19:49:39 -0500

    day 022; finished hot code reloading (auto reloads per frame)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
-((jai-ts-mode . ((custom-compile-cmd . "c:/Users/Chris/git/jai/handmade_jai/make.bat")
-              (custom-run-all-cmd . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe")
-              (custom-run-test-cmd . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe test"))))
+((jai-ts-mode . ((custom-compile-cmd . "jai c:/Users/Chris/git/jai/handmade_jai/build_handmade.jai")
+                 (custom-run-all-cmd . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe")
+                 (custom-run-test-cmd . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe test")
+                 (file-to-compile-cmd . (("win32_handmade.jai" . (compile "jai c:/Users/Chris/git/jai/handmade_jai/build_win32_handmade.jai")))))))
