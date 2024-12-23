d83987ac0d317b46a772ab1691c88e0b01795448
Christopher Poile <cpoile@gmail.com>
2024-12-22 21:58:38 -0500

    middle of day 023

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
-((jai-ts-mode . ((custom-compile-cmd . "jai c:/Users/Chris/git/jai/handmade_jai/build_handmade.jai")
+((jai-ts-mode . ((compile-all-cmd . "c:/Users/Chris/git/jai/handmade_jai/make.bat")
+                 (defualt-compile-cmd . "jai c:/Users/Chris/git/jai/handmade_jai/build_handmade.jai")
                  (custom-run-all-cmd . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe")
                  (custom-run-test-cmd . "c:/Users/Chris/git/jai/handmade_jai/build/handmade.exe test")
                  (file-to-compile-cmd . (("win32_handmade.jai" . (compile "jai c:/Users/Chris/git/jai/handmade_jai/build_win32_handmade.jai")))))))
